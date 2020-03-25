c4f5c639dccf5368d07fca5839a552851c8c1f40
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-03-25 16:24:14 -0400

    Config in preparation for tomorrow

---
+++ test/bw-integration-20200326/green-netconf.sh
@@ -0,0 +1,18 @@
+ip addr del 10.0.1.2/24 dev eth0
+ip link set dev eth0 down 
+ip addr add 10.0.1.2/24 dev eth0
+ip link set eth0 address 00:00:10:00:01:02
+ip link set dev eth0 up 
+
+ip addr del 10.0.1.3/24 dev eth1
+ip link set dev eth1 down 
+ip addr add 10.0.1.3/24 dev eth1
+ip link set eth1 address 00:00:10:00:01:03
+ip link set dev eth1 up 
+
+ip neigh add 10.0.2.3 lladdr 0:0:10:0:2:3 dev eth1
+ip neigh add 10.0.1.2 lladdr 0:0:10:0:1:2 dev eth0
+ip neigh add 10.0.1.3 lladdr 0:0:10:0:1:3 dev eth1
+ip neigh add 10.0.2.2 lladdr 0:0:10:0:2:2 dev eth0
+ip route add 10.0.2.2/32 dev eth0
+
