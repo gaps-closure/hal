6ffb077189210c1d8d0788b5d5eb23c487da3681
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-03-25 16:44:01 -0400

    add rp_filter=0 on all interfaces

--- test/bw-integration-20200326/green-netconf.sh
+++ test/bw-integration-20200326/green-netconf.sh
@@ -1,4 +1,6 @@
 sysctl -w net.ipv4.conf.eth0.rp_filter=0
+sysctl -w net.ipv4.conf.eth1.rp_filter=0
+sysctl -w net.ipv4.conf.all.rp_filter=0
 # May need to restart networking here
 ip addr del 10.0.1.2/24 dev eth0
 ip link set dev eth0 down 
@@ -12,9 +14,9 @@ ip addr add 10.0.1.3/24 dev eth1
 ip link set eth1 address 00:00:10:00:01:03
 ip link set dev eth1 up 
 
-ip neigh add 10.0.2.3 lladdr 0:0:10:0:2:3 dev eth1
-ip neigh add 10.0.1.2 lladdr 0:0:10:0:1:2 dev eth0
-ip neigh add 10.0.1.3 lladdr 0:0:10:0:1:3 dev eth1
+#ip neigh add 10.0.2.3 lladdr 0:0:10:0:2:3 dev eth1
+#ip neigh add 10.0.1.2 lladdr 0:0:10:0:1:2 dev eth0
+#ip neigh add 10.0.1.3 lladdr 0:0:10:0:1:3 dev eth1
 ip neigh add 10.0.2.2 lladdr 0:0:10:0:2:2 dev eth0
 ip route add 10.0.2.2/32 dev eth0
 
