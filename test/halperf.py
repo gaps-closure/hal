c977f4836289f31a33cf3e72b5a41864ee49065b
Michael Kaplan <mkaplan@perspectalabs.com>
2020-04-01 13:57:51 -0400

    updated halperf; removes busy sleep, output formatting; self_kill

--- test/halperf.py
+++ test/halperf.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 import argparse
+import os
+import signal
+import sys
 import time
 from ctypes import *
-from threading import Thread
+from threading import Thread, Timer, Lock
 
 # Open C shared libs to the datatypes; TODO: make spec-driven
 xdc_so = None
-gma_so = None 
+gma_so = None
+
+plock = Lock()
 
 DATA_TYP_POS = 1
 DATA_TYP_DIS = 2
@@ -34,8 +39,21 @@ class Distance(Structure):
                 ("y", c_double),
                 ("z", c_double),
                 ("t", ClosureTrailer)]
-                  
-def send(m, s, t, r, c):
+
+class RptTimer(Timer):
+    def run(self):
+        while not self.finished.is_set():
+            self.finished.wait(self.interval)
+            self.function(*self.args, **self.kwargs)
+        self.finished.set()
+
+class Stats():
+    def __init__(self):
+        self.wincnt = 0
+        self.totcnt = 0
+        self.totsec = 0
+
+def send(m, s, t, r, interval):
     # Context/Socket setup
     makesock = xdc_so.xdc_pub_socket
     makesock.restype = c_void_p
@@ -44,27 +62,41 @@ def send(m, s, t, r, c):
     #initial values
     pos = Position(-74.574489, 40.695545, 101.9, ClosureTrailer(0,0,0,0,0))
     dis = Distance(-1.021, 2.334, 0.4)
-
-    while(float(r) != 0):
-        tag = GapsTag(int(m),int(s),int(t))
-        tailer = ClosureTrailer(0,0,0,0,0)
-        if int(t) == 1:
-            pos.z += 0.1
-            adu = Position(pos.x, pos.y, pos.z, ClosureTrailer(0,0,0,0,0))
-        elif int(t) == 2:
-            dis.z += 0.1
-            adu = Distance(dis.x, dis.y, dis.z, ClosureTrailer(0,0,0,0,0))
-        else:
-            raise Exception('unsupported data typ: ' + str(t))
+    tag = GapsTag(int(m),int(s),int(t))
+    stats = Stats()
+    slock = Lock()
+    
+    if int(t) == 1:
+        adu = Position(pos.x, pos.y, pos.z, ClosureTrailer(0,0,0,0,0))
+    elif int(t) == 2:
+        adu = Distance(dis.x, dis.y, dis.z, ClosureTrailer(0,0,0,0,0))
+    else:
+        raise Exception('unsupported data typ: ' + str(t))
+    
+    def task(stats,slock):
+        adu.z += 0.1
         xdc_so.xdc_asyn_send(c_void_p(sock), pointer(adu), tag)
-        print("%f sent: [%d/%d/%d] -- (%f,%f,%f)" % (time.time(), tag.mux, tag.sec, tag.typ, adu.x, adu.y, adu.z))
-        busy_sleep(1.0/float(r))
-        if c > 0:
-          c -= 1
-          if c == 0:
-            break
+        slock.acquire()
+        stats.wincnt += 1
+        stats.totcnt += 1
+        slock.release()
 
-def recv(m, s, t):
+    def print_stats(stats,tag,slock):
+        stats.totsec += interval
+        mst = "%d/%d/%d" % (tag.mux, tag.sec, tag.typ)
+        plock.acquire()
+        print("{:5.2f} | send | {:8s} | {:5d} {:7.2f} Hz | {:8d} {:8.2f} Hz".format(stats.totsec, mst, stats.wincnt, stats.wincnt/interval, stats.totcnt, stats.totcnt / stats.totsec))
+        plock.release()
+        slock.acquire()
+        stats.wincnt = 0
+        slock.release()
+        
+    rtmr = RptTimer(1.0/float(r),task,[stats, slock])
+    rtmr.start()
+    stmr = RptTimer(interval, print_stats, [stats,tag, slock])
+    stmr.start()
+        
+def recv(m, s, t, interval):
     if int(t) == DATA_TYP_POS:
         adu = Position()
     elif int(t) == DATA_TYP_DIS:
@@ -76,24 +108,40 @@ def recv(m, s, t):
     makesock = xdc_so.xdc_sub_socket
     makesock.restype = c_void_p
     sock = makesock(tag)
-    while(1):
-        xdc_so.xdc_blocking_recv(c_void_p(sock), pointer(adu), pointer(tag))
-        print('%f recv: [%d/%d/%d] -- (%f,%f,%f)' % (time.time(), tag.mux,tag.sec,tag.typ,adu.x,adu.y,adu.z))
-        
-def busy_sleep(s):
-    start = time.time()
-    while (time.time() <  start + s):
-        pass
+    stats = Stats()
+    rlock = Lock()
 
+    def print_stats(stats,tag,rlock):
+        stats.totsec += interval
+        mst = "%d/%d/%d" % (tag.mux, tag.sec, tag.typ)
+        plock.acquire()
+        print("{:5.2f} | recv | {:8s} | {:5d} {:7.2f} Hz | {:8d} {:8.2f} Hz".format(stats.totsec, mst, stats.wincnt, stats.wincnt/interval, stats.totcnt, stats.totcnt / stats.totsec))
+        plock.release()
+        rlock.acquire()
+        stats.wincnt = 0
+        rlock.release()
+
+    stmr = RptTimer(interval,print_stats,[stats,tag,rlock])
+    stmr.start()
+
+    while True:
+        xdc_so.xdc_blocking_recv(c_void_p(sock), pointer(adu), pointer(tag))
+        rlock.acquire()
+        stats.wincnt += 1
+        stats.totcnt += 1
+        rlock.release()
+
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--send', nargs=4, action='append', metavar=('MUX', 'SEC', 'TYP', 'RATE'), help='send cross-domain flow using MUX/SEC/TYP at RATE (Hz)')
     parser.add_argument('-r', '--recv', nargs=3, action='append', metavar=('MUX', 'SEC', 'TYP'), help='recv cross-domain flow mapped to MUX/SEC/TYP')
-    parser.add_argument('-c', metavar=('COUNT'), help="packets to sent (default=no limit)", type=int, default=-1)
     parser.add_argument('-l', metavar=('PATH'), help="path to mission app shared libraries (default=../appgen)", default='../appgen')
     parser.add_argument('-x', metavar=('PATH'), help="path to libxdcomms.so (default=../api)", default='../api')
     parser.add_argument('-i', metavar=('URI'), help="in URI (default=ipc:///tmp/halpub1)", default='ipc:///tmp/halpub1')
     parser.add_argument('-o', metavar=('URI'), help="out URI (default=ipc:///tmp/halsub1)", default='ipc:///tmp/halsub1')
+    parser.add_argument('--interval', help="reporting interval, default=10s", default=10)
+    parser.add_argument('-t', help="duration of test in seconds, if not specified, runs indefinitely", default=0)
     args = parser.parse_args()
 
     xdc_so = CDLL(args.x + '/libxdcomms.so', use_errno=True)
@@ -108,13 +156,32 @@ if __name__ == '__main__':
     xdc_so.xdc_register(gma_so.position_data_encode, gma_so.position_data_decode, DATA_TYP_POS)
     xdc_so.xdc_register(gma_so.distance_data_encode, gma_so.distance_data_decode, DATA_TYP_DIS)
 
+    threads = []
     if args.send:
         for s in args.send:
-            s.append(args.c)
+            s.append(float(args.interval))
             t = Thread(args=s, target=send)
+            threads.append(t)
             t.start()
+
     if args.recv:
         for r in args.recv:
+            r.append(float(args.interval))
             t = Thread(args=r, target=recv)
+            threads.append(t)
             t.start()
+
+    def self_kill():
+        os.kill(os.getpid(), signal.SIGKILL)
+
+    if float(args.t) != 0:
+        tm = Timer(float(args.t), self_kill)
+        tm.start()
+
+
+    while True:        
+        try:
+            time.sleep(99999)
+        except KeyboardInterrupt:
+            self_kill()
             
