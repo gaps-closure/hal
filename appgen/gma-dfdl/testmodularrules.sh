69ab06be61110859492ac53986146405b330a76d
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-04-02 12:44:31 -0400

    significant cleanups, mostly functions

---
+++ appgen/gma-dfdl/testmodularrules.sh
@@ -0,0 +1,3 @@
+saxonb-xslt -xsl:dagr.reverse-bw-stretch-modular.xsl -s:bw_write_111.infoset 
+saxonb-xslt -xsl:dagr.reverse-bw-stretch-modular.xsl -s:bw_write_221.infoset 
+saxonb-xslt -xsl:dagr.reverse-bw-stretch-modular.xsl -s:bw_write_222.infoset 
