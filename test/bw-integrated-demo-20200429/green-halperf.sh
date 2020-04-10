0e40e2f66f0bef437b4008a61e69f1cca16f88e2
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-04-10 14:29:48 -0400

    path for halperf

--- test/bw-integrated-demo-20200429/green-halperf.sh
+++ test/bw-integrated-demo-20200429/green-halperf.sh
@@ -1,3 +1,3 @@
-cd /home/$SESSION_USER/gaps/hal/test
+cd /home/$SESSION_USER/gaps/build/src/hal/test
 LD_LIBRARY_PATH=../appgen ./halperf.py -s 1 1 1 100 -r 2 2 1 -r 2 2 2 -i ipc:///tmp/halsubbwgreen -o ipc:///tmp/halpubbwgreen
 
