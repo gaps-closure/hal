3c492032dc00d583ad30d1a616158c8fdc9fc874
Anthony McAuley <amcauley@vencorelabs.com>
2020-04-14 18:16:37 -0400

    Add 95% quantile and other delay metrics

--- test/plot_halperf.py
+++ test/plot_halperf.py
@@ -46,26 +46,39 @@ def create_plot(x_array, y_array):
   plt.xlim(left=0)
   plt.xlim(right=max(x_array))
   plt.grid(True)
+  
   if args.sn_series:
     plt.xlabel(fields[0])
   else:
     plt.xlabel(fields[3])
+  t = round(float(rows[-1][3]) - float(rows[0][3]))
+  n = len(y_array)
+  p = round(float(rows[-1][0])/float(t), 1)
+  m = round(np.quantile(y_array, .50), 3)
+  h = round(np.quantile(y_array, .95), 3)
+  
   if args.loss:
     plt.ylabel("Packet diff (RX-TX SN)")
   else:
     plt.ylim(bottom=0)
     plt.ylabel("message latency (milliseconds)")
-
+    print('At', p, 'pps, delay seen by', n, 'packets over', t, 'secs: Median =',
+      m, 'ms, 95% quantile =', h, 'ms')
+      
   # c)  Configurure overall plot
-  t = round(float(rows[-1][3]) - float(rows[0][3]))
-  plt.title('HAL Performance with BE Loopback Devices over ' + str(t) + ' secs:\n' +
+
+  
+  plt.title('HAL with BE Loopback Devices. Performance of '
+    + str(n) + ' packets over ' + str(t) + ' secs:\n' +
     'green sends <1,1,1> and orange sends <2,2,1>, each at ' +
-    str(round(float(rows[-1][0])/float(t), 1)) + ' pkts/sec')
+    str(p) + ' pkts/sec')
     
+    
+
   # d) display and save plot
   plt.savefig('zz.png')
   plt.savefig(args.output_filename)
-  plt.show()
+  if args.plot:  plt.show()
 #  os.system('open ' + args.output_filename)
 
 
@@ -80,7 +93,8 @@ def set_xy_values():
     y_array = [(int(row[0])-int(row[1]))            for row in rows]   # TX-RX SN (loss)
   else:
     y_array = [(float(row[2])-float(row[3]))*1000.0 for row in rows]    # convert latency to micro-seconds
-  print('lengths', len(rows), len(x_array), len(y_array), len(fields), fields[2], fields[3]);
+    
+#  print('lengths', len(rows), len(x_array), len(y_array), len(fields), fields[2], fields[3]);
   return (x_array, y_array)
 
 
@@ -92,6 +106,7 @@ if __name__=='__main__':
   parser.add_argument('-i', '--input_filename', help='Input filename', type=str, default='plot_sn_data.csv')
   parser.add_argument('-o', '--output_filename', help='Output filename', type=str, default='plot_sn_data.png')
   parser.add_argument('-l', '--loss', help='Y-axis is packet loss (RX-TX SN)', action='store_true')
+  parser.add_argument('-p', '--plot', help='do not display results (deault = display)', action='store_false')
   parser.add_argument('-s', '--sn_series', help='X-axis is sequence number (default = time series)', action='store_true')
   args = parser.parse_args()
   
