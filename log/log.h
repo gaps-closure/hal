4063597d9271923a08deec4f861d9988e9de7289
Rajesh Krishnan <rjsh.krshnn@gmail.com>
2020-07-13 22:31:47 -0400

    Make LOG_LEVEL_MIN 0 by default, hal -l needs to be useful

--- log/log.h
+++ log/log.h
@@ -17,8 +17,8 @@ typedef void (*log_LockFn)(void *udata, int lock);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
-//#define LOG_LEVEL_MIN   0       /* Allow all logging messages */
-#define LOG_LEVEL_MIN   2       /* Increase to removes non-warning messages for better performance  */
+#define LOG_LEVEL_MIN   0      /* Use this, HAL daemon can use -l NUM to control logging level, and HAL API has its own hardcoded set_level() */
+// #define LOG_LEVEL_MIN   2   /* Undefine log_* functions above this level for slight performance boost, avoids entering function, but requires recompilation for debug!  */
 
 #if LOG_TRACE >= LOG_LEVEL_MIN
   #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
