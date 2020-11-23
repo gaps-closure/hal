c21699581c3737c9a3c8574d6e38f71d3af4bbd2
Anthony McAuley <mcauley@Tonys-MacBook-Pro.local>
2020-11-23 18:27:29 -0500

    Payload mode gets data from receive buffer after the packet

--- daemon/packetize_sdh_be_v3.c
+++ daemon/packetize_sdh_be_v3.c
@@ -16,7 +16,7 @@ void sdh_be_v3_print(pkt_sdh_be_v3 *p) {
     fprintf(stderr, "sec=%u ",        ntohl(p->message_tag));
     fprintf(stderr, "typ=%u ",        ntohl(p->data_tag));
     fprintf(stderr, "dst=%u ",        p->destination_tag);
-    fprintf(stderr, "dsc=%d ",        ntohl(p->descriptor_type));
+    fprintf(stderr, "dsc=%08x ",      p->descriptor_type);
     fprintf(stderr, "gaps=%x.%x ",    p->gaps_time_up,  p->gaps_time_lo);
     fprintf(stderr, "unix=%x.%x ",    p->linux_time_up, p->linux_time_lo);
     fprintf(stderr, "len=%d ",        ntohl(p->dma_data_len));
@@ -25,12 +25,21 @@ void sdh_be_v3_print(pkt_sdh_be_v3 *p) {
     fprintf(stderr, "\n");
 }
 
-/* Check if data length tits in an external packet */
+/* Put data address into an external packet */
 void put_dma_adresss (pkt_sdh_be_v3 *pkt, uint8_t *data_in) {
     uint64_t x = (uint64_t) data_in;
     pkt->dma_data_addr_lo = (uint32_t) (x & 0xFFFFFFFF);
     pkt->dma_data_addr_up = (uint32_t) (x >> 32);;
 //    fprintf(stderr, "addr = %p %lx (%x %x)\n",  data_in, x, pkt->dma_data_addr_up, pkt->dma_data_addr_lo);
+}
+
+/* Get data fron an external packet (placed at end of packet) */
+void get_dma_data (pkt_sdh_be_v3 *pkt, uint8_t *data_out, uint32_t  len) {
+    uint8_t  *data_in;
+
+    data_in = (uint8_t *) pkt + sizeof(*pkt);
+//    fprintf(stderr, "%s ready to copy %d bytes: %p -> %p\n", __func__, len, data_in, data_out);
+    memcpy (data_out, data_in, len);
 }
 
 /* Put data from external packet (*in) into internal HAL PDU */
@@ -41,12 +50,8 @@ void pdu_from_sdh_be_v3 (pdu *out, uint8_t *in) {
     out->psel.tag.mux = ntohl(pkt->session_tag);
     out->psel.tag.sec = ntohl(pkt->message_tag);
     out->psel.tag.typ = ntohl(pkt->data_tag);
-//    len = check_len_sdh_be_v3(ntohl(pkt->imm_data_len));
-    out->data_len     = pkt->dma_data_len;
-    
-    sdh_be_v3_print(pkt); exit(1);
-
-    memcpy (out->data, pkt->imm_data, pkt->dma_data_len);
+    out->data_len     = ntohl(pkt->dma_data_len);
+    get_dma_data(pkt, out->data, out->data_len);
 }
 
 /* Put data into external packet (*out) from internal HAL PDU */
@@ -59,8 +64,9 @@ int pdu_into_sdh_be_v3 (uint8_t *out, pdu *in, gaps_tag *otag) {
     pkt->message_tag        = htonl(otag->sec);
     pkt->data_tag           = htonl(otag->typ);
     pkt->destination_tag    = 0;
-    pkt->descriptor_type    = htonl(1); // Payload Mode - check value and if need hton
- 
+//    pkt->descriptor_type    = 0x01000000;           // Payload Mode (in test example)
+    pkt->descriptor_type    = 0x10000000;           // Payload Mode
+
     // b) Timestamps set by driver (linux) or ILIP (gaps)
 //    pkt->gaps_time_lo = htonl(0x01234567);  /* XXX: Just set for testing */
 //    pkt->gaps_time_up = htonl(0x89abcdef);  /* XXX: Just set for testing */
@@ -75,6 +81,6 @@ int pdu_into_sdh_be_v3 (uint8_t *out, pdu *in, gaps_tag *otag) {
     pkt->dma_data_len       = htonl(in->data_len);
     put_dma_adresss(pkt, in->data);
     
-//    sdh_be_v3_print(pkt); exit(1);
+//    sdh_be_v3_print(pkt); // exit(1);
     return (sizeof(*pkt));       /* v2+ always sends 256 byte packet */
 }
