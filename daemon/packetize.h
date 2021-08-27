6c804d325a6f610a9c273455fcb80deccd4ec205
Anthony McAuley <mcauley@Tonys-MacBook-Pro.local>
2021-08-27 14:57:30 -0400

    shm update

--- daemon/packetize.h
+++ daemon/packetize.h
@@ -1,10 +1,11 @@
 /* Convert between Internal HAL PDU and any external packet */
 
+#include "packetize_sdh_ha_v1.h"
 #include "packetize_sdh_be_v1.h"
 #include "packetize_sdh_be_v2.h"
 #include "packetize_sdh_be_v3.h"
 #include "packetize_sdh_bw_v1.h"
-#include "packetize_sdh_ha_v1.h"
+#include "packetize_sdh_sm_v1.h"
 
 extern int  pdu_from_packet(pdu *, uint8_t *, int, device *);
 extern void pdu_into_packet(uint8_t *, pdu *, int *, selector *, const char *);
